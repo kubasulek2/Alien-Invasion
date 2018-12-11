7b61e18e78d0f83024005f3db1574b41bec88cd9
Kuba Sukowski <kubasulek2@gmail.com>
2018-12-11 14:56:24 +0100

    gulp configured, added: fonts and sounds

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,8 @@
 $( () => {
-
+   /* const soundtrack = new Audio('../music/Quirky-Action2.mp3');
+    soundtrack.addEventListener('ended', function() {
+        this.currentTime = 0;
+        this.play();
+    }, false);
+    soundtrack.play();*/
 });
