ec03c503da833701de8979fc9c702bb2b9170558
Kuba Sukowski <kubasulek2@gmail.com>
2018-12-11 18:51:56 +0100

    shot sound and disable click for 1.5s added

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,48 @@
 $( () => {
+    const shot = new Audio('../music/shot.mp3');
    /* const soundtrack = new Audio('../music/Quirky-Action2.mp3');
     soundtrack.addEventListener('ended', function() {
         this.currentTime = 0;
         this.play();
     }, false);
     soundtrack.play();*/
-
+//find mouse position within game div, put eventlisteners on it
+    const cursor = $('#cursor');
     let mouseX = 0, mouseY = 0;
-    $('#game').on('mousemove', function(e){
-        mouseX = e.pageX - $('#game').offset().left;
-        mouseY = e.pageY - $('#game').offset().top;
-    });
+    let disabled = false;
+    const game = $('#game');
+
+
+    game.on('mousemove', function(e){
+            mouseX = e.pageX - game.offset().left;
+            mouseY = e.pageY - game.offset().top;
+        });
+    game.on('mouseenter', function () {cursor.css("display","block");
+            cursor.css("display","block");
+        });
+
+    game.on('mouseleave',function () {
+            cursor.removeAttr('style')
+        });
+
+
+    game.on('click', function (e) {
+            e.stopImmediatePropagation();
+            if (!disabled){
+                shot.play();
+            }
+
+
+            disabled = true;
+            window.setTimeout(function () {
+                disabled = false
+            },1500)
+        });
+
+
+
 
 // cache the selector
-    const cursor = $("#cursor");
     let xp = 0, yp = 0;
     const drawCursor = function(){
         xp += (mouseX - xp-25);
